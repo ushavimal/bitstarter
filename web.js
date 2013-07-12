63d7aef07f3c2f5279b06f641e23babc042f759f
Ubuntu <ubuntu@ip-10-205-10-163.ec2.internal>
2013-07-12 01:51:59 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var content = fs.readFileSync('index.html');
+var content = fs.readFileSync('index.html', 'utf8');
 app.get('/', function(request, response) {
   response.send(content);
 });
